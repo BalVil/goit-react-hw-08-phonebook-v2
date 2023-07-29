c0051123d687d2a6306e45ba1fc7c251ddf45d9e
Vital <balutavili@gmail.com>
2023-07-29 20:06:10 +0300

    set up for gh-pages

--- src/hooks/useSignUp.js
+++ src/hooks/useSignUp.js
@@ -1,5 +1,4 @@
 import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { authOperations } from 'redux/auth';
 
